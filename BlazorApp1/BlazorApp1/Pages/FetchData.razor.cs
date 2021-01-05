a76511b37454cd0da4b2e27f46067d1a079b527e
Seth Lam <sihlam@microsoft.com>
2021-01-04 18:47:52 -0800

    v1

---
+++ BlazorApp1/BlazorApp1/Pages/FetchData.razor.cs
@@ -0,0 +1,24 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Pages
+{
+    partial class FetchData : ComponentBase
+    {
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {   
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
